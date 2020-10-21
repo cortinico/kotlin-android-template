ffbfc699a5b3bee184d19c394fb95634802011f6
Nicola Corti <corti.nico@gmail.com>
2020-10-21 21:25:50 +0200

    Detekt to 1.14.2 (#21)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -17,7 +17,7 @@ object Versions {
 
 object BuildPluginsVersion {
     const val AGP = "4.0.0"
-    const val DETEKT = "1.10.0"
+    const val DETEKT = "1.14.2"
     const val KOTLIN = "1.3.72"
     const val KTLINT = "9.4.1"
     const val VERSIONS_PLUGIN = "0.28.0"
