89078adf9c119d43467e32dbc53cbeff5a381a84
goooler <wangzongler@gmail.com>
2020-12-16 10:37:56 +0800

    remove kotlin-android-extensions for adapt kotlin 1.4.20

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -18,7 +18,7 @@ object Versions {
 object BuildPluginsVersion {
     const val AGP = "4.1.1"
     const val DETEKT = "1.14.2"
-    const val KOTLIN = "1.4.20"
+    const val KOTLIN = "1.4.21"
     const val KTLINT = "9.4.1"
     const val VERSIONS_PLUGIN = "0.33.0"
 }
