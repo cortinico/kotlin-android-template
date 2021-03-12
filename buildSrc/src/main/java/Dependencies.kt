4bd33b1fed256c87f3608b10b005fbd4cde8b494
Kevin Stieglitz <kevin.stieglitz@jambit.com>
2021-03-12 15:27:46 +0100

    Use detekt-formatting plugin instead of ktlint

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -19,7 +19,6 @@ object BuildPluginsVersion {
     const val AGP = "4.1.2"
     const val DETEKT = "1.15.0"
     const val KOTLIN = "1.4.30"
-    const val KTLINT = "9.4.1"
     const val VERSIONS_PLUGIN = "0.36.0"
 }
 
