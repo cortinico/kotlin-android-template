0a2577d791b91aa5900ad6f14113a75f0c7edb86
Chao Zhang <zhangchao6865@gmail.com>
2021-03-16 12:34:14 -0700

    Upgrade Detekt to 1.16.0 (#37)
    Co-authored-by: Nicola Corti <corti.nico@gmail.com>

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -17,7 +17,7 @@ object Versions {
 
 object BuildPluginsVersion {
     const val AGP = "4.1.2"
-    const val DETEKT = "1.15.0"
+    const val DETEKT = "1.16.0"
     const val KOTLIN = "1.4.30"
     const val KTLINT = "9.4.1"
     const val VERSIONS_PLUGIN = "0.36.0"
