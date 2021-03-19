363cf83f317de3112d8de8bd7e5a7a6f69bb33df
Goooler <wangzongler@gmail.com>
2021-03-19 10:06:53 +0800

    Apply `com.github.ben-manes.versions` for all projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ subprojects {
     apply {
         plugin("io.gitlab.arturbosch.detekt")
         plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("com.github.ben-manes.versions")
     }
 
     ktlint {
