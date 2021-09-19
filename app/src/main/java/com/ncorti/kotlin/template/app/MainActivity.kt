70d10e7e8cf2de051b4347f7d62e0cb76b6736c3
Abhishek Dubey <abhishekdubey331@gmail.com>
2021-09-19 18:11:16 +0530

    Added empty for no number provided for factorial

--- app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
+++ app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ncorti.kotlin.template.app
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ncorti.kotlin.template.app.databinding.ActivityMainBinding
 import com.ncorti.kotlin.template.library.FactorialCalculator
@@ -18,17 +19,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonCompute.setOnClickListener {
-            val input = binding.editTextFactorial.text.toString().toInt()
-            val result = FactorialCalculator.computeFactorial(input).toString()
+            if (binding.editTextFactorial.text.isNotEmpty()) {
+                val input = binding.editTextFactorial.text.toString().toInt()
+                val result = FactorialCalculator.computeFactorial(input).toString()
 
-            binding.textResult.text = result
-            binding.textResult.visibility = View.VISIBLE
+                binding.textResult.text = result
+                binding.textResult.visibility = View.VISIBLE
 
-            notificationUtil.showNotification(
-                context = this,
-                title = getString(R.string.notification_title),
-                message = result
-            )
+                notificationUtil.showNotification(
+                    context = this,
+                    title = getString(R.string.notification_title),
+                    message = result
+                )
+            } else {
+                Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
