b1181e022ae39326aa6c45c17df9d2499fa14f29
Nelson Osacky <nosacky@gradle.com>
2021-12-29 00:21:46 +0100

    Use TOML for dokka

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,15 +6,9 @@ repositories {
     mavenCentral()
 }
 
-object Plugins {
-    const val AGP = "7.0.4"
-    const val DOKKA = "1.5.0"
-    const val KOTLIN = "1.6.10"
-}
-
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${Plugins.KOTLIN}")
-    implementation("com.android.tools.build:gradle:${Plugins.AGP}")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:${Plugins.DOKKA}")
-    implementation("org.jetbrains.dokka:dokka-core:${Plugins.DOKKA}")
+    implementation(libs.kgp)
+    implementation(libs.agp)
+    implementation(libs.dokka.gradle.plugin)
+    implementation(libs.dokka.core)
 }
---
+++ buildSrc/settings.gradle.kts
@@ -0,0 +1,8 @@
+enableFeaturePreview("VERSION_CATALOGS")
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
