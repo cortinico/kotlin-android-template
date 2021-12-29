c869bc6a1da22812104652bc993d21159afb99e8
Nelson Osacky <nosacky@gradle.com>
2021-12-29 01:11:40 +0100

    Use Java 11

--- library-android/build.gradle.kts
+++ library-android/build.gradle.kts
@@ -19,11 +19,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     buildTypes {
